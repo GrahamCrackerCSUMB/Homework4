1b8aab16c243cedd5a43ceba1e98ac0e8bf78adb
ErinH <erhurley@csumb.edu>
2026-03-30 23:42:32 -0700

    added api call

--- index.mjs
+++ index.mjs
@@ -1,15 +1,21 @@
 import express from 'express';
 import fetch from 'node-fetch';
-const planets = (await import ('npm-solarsystem')).default;
 const quotes =(await import("success-motivational-quotes")).default;
 quotes.getTodaysQuote()
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
     let quote = quotes.getTodaysQuote();
-    res.render('index', {quote});
+    let apiKey = "7756a1e81f817c186cf57294e1c19b37b49c54b8f34e7c499ee0ce5cd86cd16e";
+	let url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&featured=true&query=solar-system`;
+    let response = await fetch(url);
+    let data = await response.json();
+
+    let randomImage = data.urls.full;
+
+    res.render('index', { quote, randomImage});
 });
 
 app.get('/roles', (req, res) => {
